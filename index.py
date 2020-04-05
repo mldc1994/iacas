9dbb4f824f0acb5aeaf2c7dcdd83f100dbd00b66
marc <marc@delacruz.com>
2020-04-05 15:17:50 +0800

    this is the flask code for iacas

--- index.py
+++ index.py
@@ -1,22 +1,18 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, request
 from werkzeug import secure_filename
-from psycopg2.extras import RealDictCursor
-from datetime import date
-from datetime import time
+import MySQLdb.cursors, os
 from datetime import datetime
-import psycopg2, os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-conn = psycopg2.connect("dbname='iacas' host='localhost' user='postgres' password='!p@ssword' port='5432'")
+db = MySQLdb.connect(host='localhost', db='trymysql', user='marc', passwd='123')
 
 @app.route("/")
 def home():
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    psql = "select author, title, content, datee, imagee, id from blog ORDER BY datee2 DESC, datee2 DESC"
-    cursor.execute(psql)
-    posts = cursor.fetchall()[:3]
+    dict_cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    dict_cursor.execute("select author, title, content, datee, imagee, id from blog ORDER BY datee2 DESC, datee2 DESC")
+    posts = dict_cursor.fetchall()[:3]
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -27,10 +23,11 @@ def about():
 def createblog():
     if request.method == 'POST':
         now = datetime.now()
-        cursor = conn.cursor()
-        author = request.form['author']
-        title = request.form['title']
-        content = request.form['content']
+        req = request.form
+        dict_cursor = db.cursor(MySQLdb.cursors.DictCursor)
+        author = req['author']
+        title = req['title']
+        content = req['content']
         datee = now.strftime("%x")
         datee2 = now.strftime("%X")
         file = request.files['imagee']
@@ -39,21 +36,18 @@ def createblog():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
         FileImage = file.filename
         image_string = format(FileImage)
-        psql = 'INSERT INTO blog (author, title, content, datee, datee2, imagee) VALUES (%s,%s,%s,%s,%s,%s)'
-        data = (author, title, content, datee, datee2, image_string)
-        cursor.execute(psql, data)
-        conn.commit()
-        cursor.close()
+        dict_cursor.execute('INSERT INTO blog (author, title, content, datee, datee2, imagee) VALUES (%s,%s,%s,%s,%s,%s)', (author, title, content, datee, datee2, image_string))
+        db.commit()
+        dict_cursor.close()
         return "<script>alert('Inserted Successfully!');window.location.href='/';</script>"
     return render_template('blogdb.html', title='create a blog')
 
 @app.route("/readmore/<id>", methods=['GET'])
 def readmore(id):
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    psql = "select * from blog where id=%s"
-    cursor.execute(psql, id)
-    blog = cursor.fetchone()
+    dict_cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    dict_cursor.execute("select * from blog where id=%s")
+    blog = dict_cursor.fetchone()
     return render_template('blogtemplate.html', title="Blog", blog=blog)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")    
+    app.run(debug=True)    
